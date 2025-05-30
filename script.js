583ceb3cdd05f75cd85849507f21932694be1a33
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-30 22:15:04 +0530

    working on functionalities

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var tl = gsap.timeline()
+var tl = gsap.timeline({paused:true})
 
 tl.to("#full",{
-    right:"0"
+    right:"0",
+    duration:0.6
+})
+tl.from("#full h4",{
+    x:150,
+    duration:1,
+    stagger:0.3,
+    opacity:0
+})
+tl.from("#full i",{
+    opacity:0
 })
