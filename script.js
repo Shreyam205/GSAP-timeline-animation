809f794ef43c00bfe914cc71f2a2247639102a41
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-30 22:27:34 +0530

    functionalities are done

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var tl = gsap.timeline({paused:true})
+var menu = document.querySelector("#nav i")
+var cross = document.querySelector("#full i")
+
+var tl = gsap.timeline()
 
 tl.to("#full",{
     right:"0",
-    duration:0.6
+    duration:0.4
 })
 tl.from("#full h4",{
     x:150,
-    duration:1,
+    duration:0.5,
     stagger:0.3,
     opacity:0
 })
 tl.from("#full i",{
     opacity:0
+})
+
+tl.pause()
+
+menu.addEventListener("click",function(){
+    tl.play()
+})
+cross.addEventListener("click",function(){
+    tl.reverse()
 })
